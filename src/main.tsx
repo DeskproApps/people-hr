52cab9b201b3ca335d6c3bf624bf7ee54864dc6d
Paul Hutchinson <paulhappyhutchinson@protonmail.ch>
2025-05-28 15:16:13 +0000

    chore(sc-65330): Improve error reporting for apps by reporting to Sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-import * as Sentry from '@sentry/react';
 import './instrument';
 import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
@@ -15,8 +14,11 @@ import "@deskpro/deskpro-ui/dist/deskpro-custom-icons.css";
 import "./main.css";
 import "simplebar/dist/simplebar.min.css";
 import { Scrollbar } from "@deskpro/deskpro-ui";
+import { ErrorBoundary, reactErrorHandler } from '@sentry/react';
 
-const root = ReactDOM.createRoot(document.getElementById("root") as Element);
+const root = ReactDOM.createRoot(document.getElementById('root') as Element, {
+  onRecoverableError: reactErrorHandler(),
+});
 root.render(
   <React.StrictMode>
     <Scrollbar style={{ height: "100%", width: "100%" }}>
@@ -24,9 +26,9 @@ root.render(
         <HashRouter>
           <QueryClientProvider client={queryClient}>
             <Suspense fallback={<LoadingSpinner />}>
-              <Sentry.ErrorBoundary FallbackComponent={ErrorFallback}>
+              <ErrorBoundary fallback={ErrorFallback}>
                 <App />
-              </Sentry.ErrorBoundary>
+              </ErrorBoundary>
             </Suspense>
           </QueryClientProvider>
         </HashRouter>
