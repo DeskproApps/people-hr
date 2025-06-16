a00f88d8f595ae7933b447e60a3b3cbbd50f6bd2
Paul <1740195+HappyPaul55@users.noreply.github.com>
2025-06-16 17:59:28 +0100

    chore(sc-65330): Improve error reporting for apps by reporting to Sentry (#52)
    * Integrated Sentry for better error tracking in apps.
    
    * Added necessary Sentry configuration settings.

--- src/components/Error/ErrorFallback.tsx
+++ src/components/Error/ErrorFallback.tsx
@@ -3,14 +3,9 @@ import { Stack } from "@deskpro/deskpro-ui";
 import { PeopleHRError } from "../../services/peoplehr";
 import { ErrorBlock } from "./ErrorBlock";
 import { Container } from "../common";
-import type { FC } from "react";
-import type { FallbackProps } from "react-error-boundary";
+import { FallbackRender } from "@sentry/react";
 
-type Props = Omit<FallbackProps, "error"> & {
-  error: Error,
-};
-
-const ErrorFallback: FC<Props> = ({ error }) => {
+const ErrorFallback: FallbackRender = ({ error }) => {
   let message = "There was an error!";
   const button = null;
 
